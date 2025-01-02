b578e47647972019e798bf04d403c2bacfaa9e29
Ayush Tickoo <ayush.tickoo292000@gmail.com>
2025-01-02 19:49:31 +0530

    file restructuring+added assets+defining module for png

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+	Text,
+	View,
+} from 'react-native';
+
+import DiceOne from '../_assets/One.png';
+
+function App(): React.JSX.Element {
+
+	return (
+		<View>
+			<Text>This is some text</Text>
+		</View>
+	);
+}
+
+export default App;
