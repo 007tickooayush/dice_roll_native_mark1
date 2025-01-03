e5a77b21f005f00826e1e0393c540de3ea50cf31
Ayush Tickoo <ayush.tickoo292000@gmail.com>
2025-01-03 19:45:41 +0530

    handling image state+implented the app style+background for StatusBar

--- _utils/styles.ts
+++ _utils/styles.ts
@@ -1,7 +1,32 @@
-import { StyleSheet } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
+
+export const { width: windowWidth, height: windowHeight } = Dimensions.get('window');
+
+export const bgColor = '#FFF2F2';
 
 export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: bgColor,
+    },
     diceImage: {
-        
+        width: windowWidth * 0.4,//200,
+        height: windowWidth * 0.4,//200,
+    },
+    diceContainer: {
+
+    },
+    rollBtnText: {
+        paddingVertical: 10,
+        paddingHorizontal: 40,
+        borderWidth: 2,
+        borderRadius: 8,
+        borderColor: '#E5E0FF',
+        fontSize: 16,
+        color: '#8EA7E9',
+        fontWeight: '700',
+        textTransform: 'uppercase',
     },
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React, { PropsWithChildren } from 'react';
+import React, { PropsWithChildren, useState } from 'react';
 import {
 	ImageSourcePropType,
+	StatusBar,
 	Text,
 	View,
 } from 'react-native';
@@ -11,12 +12,16 @@ import DiceThr from '../_assets/Three.png';
 import DiceFor from '../_assets/Four.png';
 import DiceFiv from '../_assets/Five.png';
 import DiceSix from '../_assets/Six.png';
+import { bgColor, styles } from '../_utils/styles';
+import Dice from './Dice';
 
 function App(): React.JSX.Element {
-
+	const [diceImage, setDiceImage] = useState<ImageSourcePropType>(DiceOne);
 	return (
-		<View>
+		<View style={styles.container}>
+			<StatusBar backgroundColor={bgColor}/>
 			<Text>This is some text</Text>
+			<Dice imageUrl={diceImage} />
 		</View>
 	);
 }
